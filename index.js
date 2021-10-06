c813fea0140be67da5264eba4f93278f8dad97b4
piniondevs <sucmanuts420@gmail.com>
2021-10-06 06:07:51 +0000

    Boilerplated the project

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import Hapi from '@hapi/hapi';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const DB = process.env.DB;
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: PORT,
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
+    });
+
+    // Home Route
+    server.route({
+        path: '/',
+        method: 'GET',
+        handler: () => {
+            return 'Hello World';
+        }
+    })
+
+    await server.start();
+    console.log(`Server running at http://localhost:${PORT}`);
+
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+
+});
+
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => init()).catch(console.err);
